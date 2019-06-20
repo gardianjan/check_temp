4a9bf82b33820ee04bc7c67594a55da8acc6c32c
Jan Gardian <jan.gardian@ictmedia.cz>
2019-06-20 16:44:29 +0200

    Added configurable thresholds and parsing conditions

--- check_temp.py
+++ check_temp.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# Source https://github.com/Majlen/check_temp/blob/master/check_temp.py
+# Updated by Jan Gardian on 20/06/2019, removed minTemp and added warnTemp, critTemp from arguments
 
 import sys, argparse
 from pathlib import Path
@@ -12,11 +14,18 @@ def main(argv):
     minTemp = "0.0"
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', help='minimum temperature')
+    parser.add_argument('-w', help='warning temperature threshold')
+    parser.add_argument('-c', help='critical temperature threshold')
     args = (parser.parse_args())
 
-    if args.m:
-        minTemp = "%.1f" % float(args.m)
+    if args.w:
+        warnTempF = float(args.w)
+        warnTempS = "%.1f" % float(args.w)
+
+    if args.c:
+        critTempF = float(args.c)
+        critTempS = "%.1f" % float(args.c)
+
 
     hwmons = Path('/sys/class/hwmon')
     if (not hwmons.exists()):
@@ -29,9 +38,13 @@ def main(argv):
                 #into hwmon directory and puts them into its root instead. In hwmon class, symlink
                 #called device goes to the module's root directory.
                 hwmon = hwmon / 'device'
+            if (not (hwmon / 'name').exists()):
+                #For PCI cards name file does not exist and label can have this information
+                nameP = hwmon / 'label'
+            else:
+                nameP = hwmon / 'name'
 
             #Get name
-            nameP = hwmon / 'name'
             with nameP.open() as read:
                 nameStr = resolveName(read.readline().strip(), hwmon.resolve())
 
@@ -52,17 +65,23 @@ def main(argv):
 
                 #Get critical tempereature if exists (WARNING in Nagios)
                 (maxStr, maxF) = getTemp(temp, tempName + '_crit')
-                if (maxF > 0 and tempF > maxF and ret < 1):
+                if not args.w:
+                    warnTempF = maxF 
+                    warnTempS = maxStr
+                if (maxF > 0 and tempF > warnTempF and ret < 1):
                     retMsg = "WARNING: " + nameStr + "-" + labelStr + " is higher than its threshold "
                     ret = 1
 
                 #Get maximum temperature if exists (CRITICAL in Nagios)
                 (critStr, critF) = getTemp(temp, tempName + '_max')
-                if (critF > 0 and tempF > critF and ret < 2):
+                if not args.c:
+                    critTempF = critF
+                    critTempS = critStr
+                if (critF > 0 and tempF > critTempF and ret < 2):
                     retMsg = "CRITICAL: " + nameStr + "-" + labelStr + " is higher than its threshold "
                     ret = 2
 
-                outData.append("\'"+nameStr+"-"+labelStr+"\'="+tempStr+";"+critStr+";"+maxStr+";"+minTemp+";"+maxStr)
+                outData.append("\'"+nameStr+"-"+labelStr+"\'="+tempStr+";"+warnTempS+";"+critTempS+";"+minTemp+";"+maxStr)
 
                 avg = (avg * count + tempF) / (count + 1)
                 count = count + 1
@@ -85,7 +104,10 @@ def resolveName(curName, path):
     elif (parts[3] == "platform"):
         return parts[4]
     elif (parts[3].startswith("pci")):
-        pciAddr = parts[5].split(':', 1)
+        if (parts[5] == "hwmon"):
+            pciAddr = parts[4].split(':', 1)
+        else:
+            pciAddr = parts[5].split(':', 1)
         return curName + ".pci" + pciAddr[1]
     else:
         return curName + ".TODO.unknown"
